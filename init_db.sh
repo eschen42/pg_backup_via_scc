10c957fcdbb293113c8d3e3d7ae311e7edd1e927
Art Eschenlauer <eschen@alumni.princeton.edu>
2017-05-25 08:47:47 -0500

    fixing supervisord config issues

--- init_db.sh
+++ init_db.sh
@@ -2,9 +2,19 @@
 set -eu
 # initialize the primary db if it does not exist
 if [ ! -f var_lib_postgresql/pg/postgresql.conf ]; then
-  docker exec -ti -u postgres hotstandby_pg_1 bash -c "initdb -D /var/lib/postgresql/data"
+  docker exec -ti -u postgres hotstandby_pg_1 bash -c "initdb -D /var/lib/postgresql/data; supervisorctl start pg_data"
 fi
 # initialize the hot)standby if it does not exist
 if [ ! -f var_lib_postgresql/standby/postgresql.conf ]; then
-  docker exec -ti -u postgres hotstandby_pg_1 bash -c "initdb -D /var/lib/postgresql/data"
+  docker exec -ti -u postgres hotstandby_pg_1 bash -c "cd /var/lib/postgresql/data; \
+    supervisorctl stop pg_data; \
+    echo 'host    replication     postgres       trust' >> pg_hba.conf \
+    echo '# replication cannot be specified in the last line of the file!' >> pg_hba.conf \
+    echo 'wal_level = hot_standby' >> postgresql.conf \
+    echo 'archive_mode = on' >> postgresql.conf \
+    echo 'max_wal_senders = 3' >> postgresql.conf \
+    echo 'archive_command = '\''cp -i %p /export/postgresql/9.3/archive/%f'\' >> postgresql.conf \
+    supervisorctl start pg_data; \
+    pg_basebackup -D ../archive --no-password --write-recovery-conf --xlog-method=stream --dbname='user=postgres' \
+    "
 fi
