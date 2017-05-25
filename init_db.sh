b107596ecd4f4f9bb20126423372411cacfc244d
Art Eschenlauer <eschen@alumni.princeton.edu>
2017-05-25 15:59:19 -0500

    init DB to allow connection to postgres on pg from client on barman

--- init_db.sh
+++ init_db.sh
@@ -2,22 +2,25 @@
 set -eu
 # initialize the primary db if it does not exist
 if [ ! -f var_lib_postgresql/pg/postgresql.conf ]; then
-  docker exec -ti -u postgres hotstandby_pg_1 bash -c "
+  docker exec -ti -u postgres hotstandby_pg_1 bash -c '
+    DOMAIN=$( python -c "import socket;print socket.getfqdn('\''barman'\'')" | sed -e "s/.*\([.][^.]*\)$/\1/" )
     echo START initializing primary database
     set -e
     cd /var/lib/postgresql/data;
     initdb -D /var/lib/postgresql/data
-    echo 'local    replication     postgres     trust' >> pg_hba.conf
-    echo '# replication cannot be specified in the last line of the file!' >> pg_hba.conf
-    echo 'wal_level = hot_standby' >> postgresql.conf
-    echo 'archive_mode = on' >> postgresql.conf
-    echo 'max_wal_senders = 3' >> postgresql.conf
-    echo 'archive_command = '\''cp -i %p /export/postgresql/9.3/archive/%f'\' >> postgresql.conf
+    echo "host   all          postgres  $DOMAIN  trust"                         >> pg_hba.conf
+    echo "local  replication  postgres           trust"                         >> pg_hba.conf
+    echo "# replication cannot be specified in the last line of the file!"      >> pg_hba.conf
+    echo "wal_level = hot_standby"                                              >> postgresql.conf
+    echo "archive_mode = on"                                                    >> postgresql.conf
+    echo "max_wal_senders = 3"                                                  >> postgresql.conf
+    echo "archive_command = '\''cp -i %p /export/postgresql/9.3/archive/%f'\''" >> postgresql.conf
     pwd;
     ls -la;
-    supervisorctl start pg_data
+    supervisorctl start postgres
+    supervisorctl status postgres
     echo END initializing primary database
-  "
+  '
 fi
 # initialize the hot)standby if it does not exist
 if [ ! -f var_lib_postgresql/standby/postgresql.conf ]; then
--- scorch.sh
+++ scorch.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-sudo rm -rf var_lib_postgresql var_log
+set -eu
+if [ -z "$(sudo find var_lib_postgresql -name *.pid -print)" ]; then
+  echo scorching var_lib_postgresql var_log
+  sudo rm -rf var_lib_postgresql var_log
+fi
+
