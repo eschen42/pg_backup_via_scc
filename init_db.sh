a8f0c6966e89100292bb4d830dcb8a3d4106466c
Art Eschenlauer <eschen@alumni.princeton.edu>
2017-05-25 16:14:04 -0500

    put standby db on standby share; use archive share for archives

--- init_db.sh
+++ init_db.sh
@@ -8,13 +8,13 @@ if [ ! -f var_lib_postgresql/pg/postgresql.conf ]; then
     set -e
     cd /var/lib/postgresql/data;
     initdb -D /var/lib/postgresql/data
-    echo "host   all          postgres  $DOMAIN  trust"                         >> pg_hba.conf
-    echo "local  replication  postgres           trust"                         >> pg_hba.conf
-    echo "# replication cannot be specified in the last line of the file!"      >> pg_hba.conf
-    echo "wal_level = hot_standby"                                              >> postgresql.conf
-    echo "archive_mode = on"                                                    >> postgresql.conf
-    echo "max_wal_senders = 3"                                                  >> postgresql.conf
-    echo "archive_command = '\''cp -i %p /export/postgresql/9.3/archive/%f'\''" >> postgresql.conf
+    echo "host   all          postgres  $DOMAIN  trust"                      >> pg_hba.conf
+    echo "local  replication  postgres           trust"                      >> pg_hba.conf
+    echo "# replication cannot be specified in the last line of the file!"   >> pg_hba.conf
+    echo "wal_level = hot_standby"                                           >> postgresql.conf
+    echo "archive_mode = on"                                                 >> postgresql.conf
+    echo "max_wal_senders = 3"                                               >> postgresql.conf
+    echo "archive_command = '\''cp -i %p /var/lib/postgresql/archive/%f'\''" >> postgresql.conf
     pwd;
     ls -la;
     supervisorctl start postgres
@@ -25,14 +25,14 @@ fi
 # initialize the hot)standby if it does not exist
 if [ ! -f var_lib_postgresql/standby/postgresql.conf ]; then
   docker exec -ti -u postgres hotstandby_pg_1 bash -c "
-    echo START seeding archive
+    echo START seeding standby
     set -e
     cd /var/lib/postgresql/data;
     ps ajxf;
-    pg_basebackup -D ../archive --no-password --write-recovery-conf --xlog-method=stream --dbname='user=postgres'
-    cd /var/lib/postgresql/archive;
+    pg_basebackup -D ../standby --no-password --write-recovery-conf --xlog-method=stream --dbname='user=postgres'
+    cd /var/lib/postgresql/standby;
     pwd;
     ls -la;
-    echo END seeding archive
+    echo END seeding standby
   "
 fi
