bf0693abbdee26767867cc4c0da2699665a30b9e
Art Eschenlauer <eschen@alumni.princeton.edu>
2017-05-25 21:18:33 -0500

    refactor to try to make seeding the standby database more robust

--- init_db.sh
+++ init_db.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
+
+# much of this was inspired by https://wiki.postgresql.org/wiki/Hot_Standby
+
 set -eu
+
 # initialize the primary db if it does not exist
 if [ ! -f var_lib_postgresql/pg/postgresql.conf ]; then
   docker exec -ti -u postgres hotstandby_pg_1 bash -c '
-    DOMAIN=$( python -c "import socket;print socket.getfqdn('\''barman'\'')" | sed -e "s/.*\([.][^.]*\)$/\1/" )
     echo START initializing primary database
     set -e
     cd /var/lib/postgresql/data;
     initdb -D /var/lib/postgresql/data
-    echo "host   all          postgres  $DOMAIN  trust"                      >> pg_hba.conf
-    echo "local  replication  postgres           trust"                      >> pg_hba.conf
-    echo "# replication cannot be specified in the last line of the file!"   >> pg_hba.conf
-    echo "wal_level = hot_standby"                                           >> postgresql.conf
-    echo "archive_mode = on"                                                 >> postgresql.conf
-    echo "max_wal_senders = 3"                                               >> postgresql.conf
-    echo "archive_command = '\''cp -i %p /var/lib/postgresql/archive/%f'\''" >> postgresql.conf
     pwd;
     ls -la;
     supervisorctl start postgres
@@ -22,7 +18,47 @@ if [ ! -f var_lib_postgresql/pg/postgresql.conf ]; then
     echo END initializing primary database
   '
 fi
-# initialize the hot)standby if it does not exist
+
+# set up configuration files to initialize the hot standby
+if [ -f var_lib_postgresql/pg/postgresql.conf -a ! -f var_lib_postgresql/standby/postgresql.conf ]; then
+  docker exec -ti -u postgres hotstandby_pg_1 bash -c '
+    DOMAIN=$( python -c "import socket;print socket.getfqdn('\''barman'\'')" | sed -e "s/.*\([.][^.]*\)$/\1/" )
+    echo START configuring primary database to support initialization of hot backup
+    set -e
+
+    echo stop the database to rewrite the configuration files
+    supervisorctl stop postgres
+    supervisorctl status postgres
+    cd /var/lib/postgresql/data;
+
+    echo delete and replace lines of pg_hba.conf necessary to seed the hot standby
+    sed -i -n -e "/b506ea2f41e284465f06c2a7e7dcd561/,/7e9eb140fa111f4af6aae120c59845fc/ d; p" pg_hba.conf
+    echo "## b506ea2f41e284465f06c2a7e7dcd561 ##"                            >> pg_hba.conf
+    echo "host  all  postgres  $DOMAIN  trust"                               >> pg_hba.conf
+    echo "local  replication  postgres  trust"                               >> pg_hba.conf
+    echo "# replication cannot be specified in the last line of the file!"   >> pg_hba.conf
+    echo "## 7e9eb140fa111f4af6aae120c59845fc ##"                            >> pg_hba.conf
+
+    echo delete and replace lines of postgresql.conf necessary to seed the hot standby
+    sed -i -n -e "/b506ea2f41e284465f06c2a7e7dcd561/,/7e9eb140fa111f4af6aae120c59845fc/ d; p" postgresql.conf
+    echo "## b506ea2f41e284465f06c2a7e7dcd561 ##"                            >> postgresql.conf
+    echo "wal_level = hot_standby"                                           >> postgresql.conf
+    echo "archive_mode = on"                                                 >> postgresql.conf
+    echo "max_wal_senders = 3"                                               >> postgresql.conf
+    echo "archive_command = '\''cp -i %p /var/lib/postgresql/archive/%f'\''" >> postgresql.conf
+    echo "## 7e9eb140fa111f4af6aae120c59845fc ##"                            >> postgresql.conf
+
+    pwd;
+    ls -la;
+    echo start the database to read the rewritten configuration files
+    supervisorctl start postgres
+    supervisorctl status postgres
+
+    echo END configuring primary database to support initialization of hot backup
+  '
+fi
+
+# initialize the hot standby only if it does not exist
 if [ ! -f var_lib_postgresql/standby/postgresql.conf ]; then
   docker exec -ti -u postgres hotstandby_pg_1 bash -c "
     echo START seeding standby
@@ -33,6 +69,7 @@ if [ ! -f var_lib_postgresql/standby/postgresql.conf ]; then
     cd /var/lib/postgresql/standby;
     pwd;
     ls -la;
+    supervisorctl status postgres
     echo END seeding standby
   "
 fi
