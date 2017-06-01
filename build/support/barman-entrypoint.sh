f028e838c5eccd26f785e342b39ac347669ef372
Arthur Eschenlauer <esch0041@umn.edu>
2017-05-31 20:41:20 -0500

    Docker Client:  Version:      1.13.1  API version:  1.26  Go version:   go1.7.5  Git commit:   092cba3  Built:        Wed Feb  8 06:36:34 2017  OS/Arch:      linux/amd64
    Docker Server:
     Version:      1.13.1
     API version:  1.26 (minimum version 1.12)
     Go version:   go1.7.5
     Git commit:   092cba3
     Built:        Wed Feb  8 06:36:34 2017
     OS/Arch:      linux/amd64
     Experimental: false
    
    docker-compose version 1.10.0, build 4bd6f1a
    
    Changes to support the above

---
+++ build/support/barman-entrypoint.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# vim:set noet:
+
+echo using /usr/local/support/barman-entrypoint.sh
+
+echo This script will fail if not running as root within the Docker container.
+
+set -eo pipefail
+
+        # function configure_ssh {
+	# install -d -m 0755 -o root -g root /var/run/sshd
+        # if [[ -n ${SSH_HOST_KEY} ]] && [[ -f ${SSH_HOST_KEY} ]]; then
+        # 	echo "Installing SSH host key"
+        # 	rm -f /etc/ssh/ssh_host_*_key*
+        # 	install -m 0400 -o root -g root ${SSH_HOST_KEY} /etc/ssh/
+        # 	sed -i '/^HostKey[[:space:]]/ d' /etc/ssh/sshd_config
+        # 	echo "HostKey /etc/ssh/$(basename ${SSH_HOST_KEY})" >> /etc/ssh/sshd_config
+        # else
+        # 	echo "WARNING: Unable to install SSH host key.  SSH_HOST_KEY is not defined or file does not exist."
+        # fi
+        # }
+
+function mk_chown_dir {
+	if [ ! -d $1 ]; then mkdir -p i              $1; fi
+	if [   -d $1 ]; then chown postgres:postgres $1; fi
+}
+
+function wait_pid {
+	while kill -0 $1
+	do sleep 1
+	done
+}
+
+mk_chown_dir /var/log/supervisor
+mk_chown_dir /var/log/supervisor/cron
+mk_chown_dir /var/log/supervisor/sshd
+mk_chown_dir /var/log/supervisor/postgres
+
+mk_chown_dir /var/lib/postgresql
+mk_chown_dir /var/lib/postgresql/data
+mk_chown_dir /var/lib/postgresql/backup
+
+if [ ! -d /usr/local/support ]; then mkdir /usr/local/support; chown postgres:postgres /usr/local/support ; fi
+
+sed -n -e '/nodaemon=true/d; p' -i /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
+sleep 1
+chgrp postgres /var/run/supervisor.sock
+chmod g+rwx /var/run/supervisor.sock
+
+if [[ $# -eq 0 ]]; then
+	# configure_ssh
+
+	echo Waiting for process supervisor process, pid $(cat /var/run/supervisord.pid) to exit.
+	ls -l /run
+	gosu postgres supervisorctl status
+	echo If you are running interactively, you can press control-C to abort.
+	wait_pid $(cat /var/run/supervisord.pid)
+else
+	exec gosu postgres "$@"
+fi
