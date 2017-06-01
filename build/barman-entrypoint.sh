3c8a3390428cc92400f0d5621b5f395f85dfd331
Arthur Eshcnelauer <esch0041@umn.edu>
2017-05-31 20:36:17 -0500

    Fixes to support
    Docker Client:
     Version:      1.13.1
     API version:  1.26
     Go version:   go1.7.5
     Git commit:   092cba3
     Built:        Wed Feb  8 06:36:34 2017
     OS/Arch:      linux/amd64
    
    Docker Server:
     Version:      1.13.1
     API version:  1.26 (minimum version 1.12)
     Go version:   go1.7.5
     Git commit:   092cba3
     Built:        Wed Feb  8 06:36:34 2017
     OS/Arch:      linux/amd64
     Experimental: false
    
    docker-compose version 1.10.0, build 4bd6f1a
    
    modified:   build/barman-entrypoint.sh
    modified:   build/supervisor_sshd.conf
    modified:   build/support/primary/init_db.sh
    modified:   docker-compose.yml
    added:	build/support/barman-entrypoint.sh
    added:	image_check.sh

--- build/barman-entrypoint.sh
+++ build/barman-entrypoint.sh
@@ -1,34 +1,66 @@
 #!/bin/bash
 # vim:set noet:
 
+if [ -d /usr/local/support -a -f /usr/local/support/barman-entrypoint.sh ]; then
+  bash -c "/usr/local/support/barman-entrypoint.sh $@"
+  exit 0
+fi
+
+echo using image version of barman-entrypoint.sh
+
+echo This script will fail if not running as root within the Docker container.
+
 set -eo pipefail
 
-function configure_ssh {
-	install -d -m 0755 -o root -g root /var/run/sshd
-  # if [[ -n ${SSH_HOST_KEY} ]] && [[ -f ${SSH_HOST_KEY} ]]; then
-  # 	echo "Installing SSH host key"
-  # 	rm -f /etc/ssh/ssh_host_*_key*
-  # 	install -m 0400 -o root -g root ${SSH_HOST_KEY} /etc/ssh/
-  # 	sed -i '/^HostKey[[:space:]]/ d' /etc/ssh/sshd_config
-  # 	echo "HostKey /etc/ssh/$(basename ${SSH_HOST_KEY})" >> /etc/ssh/sshd_config
-  # else
-  # 	echo "WARNING: Unable to install SSH host key.  SSH_HOST_KEY is not defined or file does not exist."
-  # fi
+        # function configure_ssh {
+	# install -d -m 0755 -o root -g root /var/run/sshd
+        # if [[ -n ${SSH_HOST_KEY} ]] && [[ -f ${SSH_HOST_KEY} ]]; then
+        # 	echo "Installing SSH host key"
+        # 	rm -f /etc/ssh/ssh_host_*_key*
+        # 	install -m 0400 -o root -g root ${SSH_HOST_KEY} /etc/ssh/
+        # 	sed -i '/^HostKey[[:space:]]/ d' /etc/ssh/sshd_config
+        # 	echo "HostKey /etc/ssh/$(basename ${SSH_HOST_KEY})" >> /etc/ssh/sshd_config
+        # else
+        # 	echo "WARNING: Unable to install SSH host key.  SSH_HOST_KEY is not defined or file does not exist."
+        # fi
+        # }
+
+function mk_chown_dir {
+	if [ ! -d $1 ]; then mkdir -p i              $1; fi
+	if [   -d $1 ]; then chown postgres:postgres $1; fi
 }
 
+function wait_pid {
+	while kill -0 $1
+	do sleep 1
+	done
+}
+
+mk_chown_dir /var/log/supervisor
+mk_chown_dir /var/log/supervisor/cron
+mk_chown_dir /var/log/supervisor/sshd
+mk_chown_dir /var/log/supervisor/postgres
+
+mk_chown_dir /var/lib/postgresql
+mk_chown_dir /var/lib/postgresql/data
+mk_chown_dir /var/lib/postgresql/backup
+
+if [ ! -d /usr/local/support ]; then mkdir /usr/local/support; chown postgres:postgres /usr/local/support ; fi
+
+sed -n -e '/nodaemon=true/d; p' -i /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
+sleep 1
+chgrp postgres /var/run/supervisor.sock
+chmod g+rwx /var/run/supervisor.sock
+
 if [[ $# -eq 0 ]]; then
-	configure_ssh
-	mkdir -p /var/log/supervisor/sshd
-	mkdir -p /var/log/supervisor/cron
-	mkdir -p /var/log/supervisor/postgres
-	if [ ! -d /var/lib/postgresql      ]; then mkdir /var/lib/postgresql;                chown postgres:postgres /var/lib/postgresql ; fi
-	if [ ! -d /var/lib/postgresql/data ]; then mkdir /var/lib/postgresql/data                                                        ; fi
-	if [   -d /var/lib/postgresql/data ]; then                                      chown postgres:postgres /var/lib/postgresql/data ; fi
-	if [ ! -d /var/lib/postgresql/backup ]; then mkdir /var/lib/postgresql/backup                                                    ; fi
-	if [   -d /var/lib/postgresql/backup ]; then                                  chown postgres:postgres /var/lib/postgresql/backup ; fi
-	if [ ! -d /usr/local/support ]; then mkdir /usr/local/support                                                                    ; fi
-	if [   -d /usr/local/support ]; then                                                  chown postgres:postgres /usr/local/support ; fi
-	exec supervisord -c /etc/supervisord.conf
+	# configure_ssh
+
+	echo Waiting for process supervisor process, pid $(cat /var/run/supervisord.pid) to exit.
+	ls -l /run
+	gosu postgres supervisorctl status
+	echo If you are running interactively, you can press control-C to abort.
+	wait_pid $(cat /var/run/supervisord.pid)
 else
-	exec "$@"
+	exec gosu postgres "$@"
 fi
--- build/support/primary/init_db.sh
+++ build/support/primary/init_db.sh
@@ -4,21 +4,24 @@
 
     echo ---
     echo START initializing primary database on "pg" = $HOSTNAME = $MYIFADDR
+    echo "       running as $(whoami)"
 
     set -e
     cd /var/lib/postgresql/data;
-    echo "initdb -D /var/lib/postgresql/data" | tee | bash
+    initdb -D /var/lib/postgresql/data
 
-    echo `pwd` @ $HOSTNAME
+    echo "The contents of `pwd` @ $HOSTNAME are:"
     ls -la | grep -E "(conf$)|(pid$)"
 
     if [ ! -f postmaster.pid ]; then
       # ensure permissions to start the DB
       chmod 700 /var/lib/postgresql/data
-      echo starting the PostgreSQL database after "initialize the primary db if it does not exist"
+      echo Starting the PostgreSQL database after "initialize the primary db if it does not exist"
       supervisorctl start postgres
+      echo The status of the PostgreSQL database after "supervisorctl start postgres" is:
       supervisorctl status postgres
     fi
 
     echo END initializing primary database on "pg" = $HOSTNAME = $MYIFADDR
     echo ...
+
